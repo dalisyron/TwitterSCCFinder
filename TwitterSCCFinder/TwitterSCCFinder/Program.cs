8f56a03a30b7723a73863803ca7ed41b3fe1303e
IUST <ir.ac.iust@gmail.com>
2019-01-31 16:37:37 +0330

    Build social graph or load it from database if it already exists

--- TwitterSCCFinder/TwitterSCCFinder/Program.cs
+++ TwitterSCCFinder/TwitterSCCFinder/Program.cs
@@ -1,8 +1,10 @@
 ﻿using LinqToTwitter;
 using System;
-using System.Diagnostics;
-using System.Linq;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
 
 namespace TwitterTest
 {
@@ -10,48 +12,100 @@ namespace TwitterTest
     {
         private static TwitterContext twC;
 
-        static List <string> getFriends(string user_name)
-        {
-            var user = twC.User.Where(u => u.Type == UserType.Show && u.ScreenName == user_name)
-                .SingleOrDefault();
+        static List<string> user_names;
 
+        static List<string> getFriends(string user_name)
+        {
             var followerBy100s = twC.Friendship
-                .Where(f => f.Type == FriendshipType.FriendIDs && f.ScreenName == user.ScreenName)
+                .Where(f => f.Type == FriendshipType.FriendIDs && f.ScreenName == user_name)
                 .SingleOrDefault()?.IDInfo.IDs
                 .Select((id, idx) => new { id, idx })
                 .GroupBy(e => e.idx / 100)
                 .Select(g => string.Join(",", g.Select(e => e.id)));
-
-            var result = followerBy100s.SelectMany(ids => twC.User.Where(u => u.Type == UserType.Lookup && u.UserIdList == ids));
             List<string> res = new List<string>();
-            
-            foreach (User r in result)
+            foreach (string s in followerBy100s)
             {
-                res.Add(r.ScreenNameResponse);
+                res.Add(s);
             }
-            return res;
+            if (res.Count > 0)
+            {
+                return res[0].Split(',').ToList();
+            }
+            else
+            {
+                return new List<string>();
+            }
         }
 
-        static List <string> getFollowers(string user_name)
+        static List<string> getFriendsByID(string idt)
         {
-            var user = twC.User.Where(u => u.Type == UserType.Show && u.ScreenName == user_name)
-                .SingleOrDefault();
-
             var followerBy100s = twC.Friendship
-                .Where(f => f.Type == FriendshipType.FollowerIDs && f.ScreenName == user.ScreenName)
+                .Where(f => f.Type == FriendshipType.FriendIDs && f.UserID == idt)
                 .SingleOrDefault()?.IDInfo.IDs
                 .Select((id, idx) => new { id, idx })
                 .GroupBy(e => e.idx / 100)
                 .Select(g => string.Join(",", g.Select(e => e.id)));
-
-            var result = followerBy100s.SelectMany(ids => twC.User.Where(u => u.Type == UserType.Lookup && u.UserIdList == ids));
             List<string> res = new List<string>();
-
-            foreach (User r in result)
+            foreach (string s in followerBy100s)
             {
-                res.Add(r.ScreenNameResponse);
+                res.Add(s);
             }
-            return res;
+            if (res.Count > 0)
+            {
+                return res[0].Split(',').ToList();
+            }
+            else
+            {
+                return new List<string>();
+            }
+        }
+        static List<string> getFollowers(string user_name)
+        {
+
+
+            var followerBy100s = twC.Friendship
+                .Where(f => f.Type == FriendshipType.FollowerIDs && f.ScreenName == user_name)
+                .SingleOrDefault()?.IDInfo.IDs
+                .Select((id, idx) => new { id, idx })
+                .GroupBy(e => e.idx / 100)
+                .Select(g => string.Join(",", g.Select(e => e.id)));
+            List<string> res = new List<string>();
+            foreach (string s in followerBy100s)
+            {
+                res.Add(s);
+            }
+            if (res.Count > 0)
+            {
+                return res[0].Split(',').ToList();
+            }
+            else
+            {
+                return new List<string>();
+            }
+        }
+        static List<string> getFollowersByID(string idt)
+        {
+
+
+            var followerBy100s = twC.Friendship
+                .Where(f => f.Type == FriendshipType.FollowerIDs && f.UserID == idt)
+                .SingleOrDefault()?.IDInfo.IDs
+                .Select((id, idx) => new { id, idx })
+                .GroupBy(e => e.idx / 100)
+                .Select(g => string.Join(",", g.Select(e => e.id)));
+            List<string> res = new List<string>();
+            foreach (string s in followerBy100s)
+            {
+                res.Add(s);
+            }
+            if (res.Count > 0)
+            {
+                return res[0].Split(',').ToList();
+            }
+            else
+            {
+                return new List<string>();
+            }
         }
 
         static bool Follows(string source_user, string dest_user)
@@ -67,12 +121,176 @@ namespace TwitterTest
             return friendship.TargetRelationship.FollowedBy;
         }
 
+        static List <string> Propagate(List <string> src_users, int criteria)
+        {
+            List<string> add = new List<string>();
+            SortedDictionary<string, bool> sourceMark = new SortedDictionary<string, bool>();
+            SortedDictionary<string, bool> candidMark = new SortedDictionary<string, bool>();
+            SortedDictionary<string, int> sourceCount = new SortedDictionary<string, int>();
+            SortedDictionary<string, bool> sinkFlag = new SortedDictionary<string, bool>();
+            List<string> candidates = new List<string>();
+
+            foreach (string user in src_users)
+            {
+                sourceMark[user] = true;
+            }
+            foreach (string user in src_users)
+            {
+                List<string> friends = new List<string>();
+                while (friends.Count == 0)
+                {
+                    try
+                    {
+                        friends = getFriendsByID(user);
+                        break;
+                    }
+                    catch (System.AggregateException ex)
+                    {
+                        var x = ex.InnerException;
+                        string err = x.ToString().Split('\n')[0];
+                        if (err.Contains("Rate limit"))
+                        {
+                            Console.WriteLine(err);
+                            Console.WriteLine("Waiting 15 minutes for new API call window");
+                            Thread.Sleep(60 * 15 * 1000);
+                            Console.WriteLine("Done sleeping");
+                        } else
+                        {
+                            break;
+                        }
+                    }
+                }
+                Console.Write("current user : ");
+                Console.WriteLine(user);
+                foreach (string f in friends) {
+                    if (!sourceMark.ContainsKey(f))
+                    {
+                        if (sourceCount.ContainsKey(f))
+                        {
+                            sourceCount[f] += 1;
+                        } else
+                        {
+                            sourceCount[f] = 1;
+                        }
+                        if (!candidMark.ContainsKey(f)) {
+                            candidates.Add(f);
+                            candidMark[f] = true;
+                        }
+                    }
+                }
+            }
+            
+            foreach (string u in candidates)
+            {
+                if (sourceCount[u] > criteria)
+                {
+                    add.Add(u);
+                }
+            }
+            List<string> res = new List<string>();
+            foreach (string u in src_users)
+            {
+                res.Add(u);
+            }
+            foreach (string u in add)
+            {
+                res.Add(u);
+            }
+            return res;
+        }
+
+        static Dictionary<string, List <string>> getAdjacencyList(List <string> users)
+        {
+            Dictionary<string, List<string>> adj = new Dictionary<string, List<string>>();
+            SortedDictionary<string, bool> src_mark = new SortedDictionary<string, bool>();
+            foreach (string user in users)
+            {
+                src_mark[user] = true;
+            }
+            foreach (string user in users)
+            {
+                List<string> friends = new List<string>();
+                while (friends.Count == 0)
+                {
+                    try
+                    {
+                        friends = getFriendsByID(user);
+                        List<string> tmp = new List<string>();
+                        foreach (string u in friends)
+                        {
+                            if (src_mark.ContainsKey(u))
+                            {
+                                tmp.Add(u);
+                            }
+                        }
+                        adj[user] = tmp;
+                        break;
+                    }
+                    catch (System.AggregateException ex)
+                    {
+                        var x = ex.InnerException;
+                        string err = x.ToString().Split('\n')[0];
+                        if (err.Contains("Rate limit"))
+                        {
+                            Console.WriteLine(err);
+                            Console.WriteLine("Waiting 15 minutes for new API call window");
+                            Thread.Sleep(60 * 15 * 1000);
+                            Console.WriteLine("Done sleeping");
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            return adj;
+        }
         static void Main(string[] args)
         {
             IAuthorizer token = GetOauthToken();
             twC = new TwitterContext(token);
 
-            
+            List<string> init_users = new List<string>();
+            List<string> user_ids = new List<string>();
+            Dictionary<string, List<string>> adjacencyList = new Dictionary<string, List<string>>();
+
+            if (File.Exists(TwitterSCCFinder.Settings.data_path + "\\initusers.txt"))
+            {
+                Console.WriteLine("Located initusers history");
+                init_users = File.ReadAllText(TwitterSCCFinder.Settings.data_path + "\\initusers.txt").Split('\n').ToArray().ToList();
+            }
+            else
+            {
+                init_users = getFriends("DevCampIUST");
+            }
+
+            if (File.Exists(TwitterSCCFinder.Settings.data_path + "\\propagate1.txt"))
+            {
+                Console.WriteLine("Located propagate history");
+                user_ids = File.ReadAllText(TwitterSCCFinder.Settings.data_path + "\\propagate1.txt").Split('\n').ToArray().ToList();
+            }
+            else
+            {
+                user_ids = Propagate(init_users, 5);
+            }
+
+            if (File.Exists("jaklsdfjklajsdklfjalskddjfl.txt"))
+            {
+
+            } else
+            {
+                adjacencyList = getAdjacencyList(user_ids);
+            }
+            foreach (KeyValuePair<string, List<string>> user in adjacencyList)
+            {
+                Console.Write(user.Key + " ");
+                foreach(string v in user.Value)
+                {
+                    Console.Write(v + ",");
+                }
+                Console.WriteLine("---");
+            }
             Console.Read();
         }
 
---
+++ TwitterSCCFinder/TwitterSCCFinder/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterSCCFinder
+{
+    public class Settings
+    {
+        public static string data_path = "C:\\Users\\mobin\\Desktop\\TwitterSCCFinder\\TwitterSCCFinder\\TwitterSCCFinder\\Data";
+    }
+}
